50d622322c4e50686a72b74fb8027d8099525a90
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-03-29 15:39:32 -0300

    fix limitação de casas decimais

--- main.js
+++ main.js
@@ -49,7 +49,8 @@ function atualizarTabela() {
 function atulizarMediaFinal() {
   const mediaFinal = calcularMediaFinal();
 
-  document.getElementById("media-final-valor").innerHTML = mediaFinal;
+  document.getElementById("media-final-valor").innerHTML =
+    mediaFinal.toFixed(2);
   document.getElementById("media-final-resultado").innerHTML =
     mediaFinal >= 7 ? spanAprovado : spanReprovado;
 }
